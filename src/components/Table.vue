40caf1f7cded87ace89554a6398015ddd5756472
“jiafen” <qw159321@wisenvr.com>
2024-10-25 17:26:57 +0800

    調整table樣式

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,8 +1,8 @@
 <template>
-    <h2 class="text-center font-bold text-3xl p-2 text-gray-500">{{ tableTitle }}</h2>
-    <!-- <el-main class="bg-white"> -->
-        <el-table v-if="tableHeader.length !== 0" :data="tableData" class="bg-white rounded-lg shadow text-lg p-4" :header-cell-style="{ whiteSpace: 'nowrap' }">
-            <el-table-column v-for="(col, index) in tableHeader" :prop="col" :label="col" :key="index"  :min-width="140"/>
+    <h2 class="text-center font-bold text-3xl p-2 text-gray-500 mb-4">{{ tableTitle }}</h2>
+        <el-table ref="multipleTableRef" border  v-if="tableHeader.length !== 0" :data="tableData" class="bg-white rounded-lg shadow text-lg"  style="width: 96%" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="40" />
+            <el-table-column v-for="(col, index) in tableHeader" :prop="col" :label="col" :key="index"  :min-width="142" />
             <el-table-column label="edit">
                 <template v-slot="scope">
                     <el-button @click="$emit('openEditModal', scope.row)" type="primary" class="bg-gray-500 hover:bg-gray-600 border-gray-500 hover:border-gray-600">
@@ -12,10 +12,11 @@
             </el-table-column>
         </el-table>
         <p v-else class="text-center text-lg text-gray-500">No data</p>
-    <!-- </el-main> -->
 </template>
 
 <script setup>
+
+    import { ref } from 'vue'
     import { Edit } from '@element-plus/icons-vue'
     defineProps({
         tableData: {
@@ -32,4 +33,11 @@
         }
     })
     defineEmits(['openEditModal'])
+
+    const multipleTableRef = ref()
+    const multipleSelection = ref([])
+    const handleSelectionChange = (val) => {
+        multipleSelection.value = val
+    }
+
 </script>
